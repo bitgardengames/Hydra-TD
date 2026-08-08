31d10ae4217b1359b75e0fb9835d0d09ea5dda77
bitgardengames <bitgardengames@gmail.com>
2026-08-08 10:31:18 -0600

    Remove test scripts

--- tests/run.lua
+++
@@ -1,5 +0,0 @@
-package.path = "./?.lua;./?/init.lua;" .. package.path
-
-dofile("tests/waves_boss_adds_test.lua")
-dofile("tests/campaign_wave_balance_test.lua")
-dofile("tests/save_normalization_test.lua")
