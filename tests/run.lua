bfb7a7c1c61c28e2ce5eefb08dee865af37d3df9
bitgardengames <bitgardengames@gmail.com>
2026-08-08 10:21:59 -0600

    Simplify save data normalization

--- tests/run.lua
+++ tests/run.lua
@@ -2,3 +2,4 @@ package.path = "./?.lua;./?/init.lua;" .. package.path
 
 dofile("tests/waves_boss_adds_test.lua")
 dofile("tests/campaign_wave_balance_test.lua")
+dofile("tests/save_normalization_test.lua")
